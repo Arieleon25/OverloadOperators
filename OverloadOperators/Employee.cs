940577e48967ade7216ed1db457038efbbb3a1a8
Arieleon25 <Arieleon25@yahoo.com>
2023-03-17 13:04:24 -0400

    Add project files.

---
+++ OverloadOperators/Employee.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadOperators
+{
+    public class Employee : Person 
+    {
+        public int ID { get; set; }
+
+        public static bool operator ==(Employee employee, Employee employee2)
+        {
+            if (employee.ID == employee2.ID)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator !=(Employee employee, Employee employee2)
+        {
+            if (employee.ID == employee2.ID)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            Person person = obj as Person;
+            if (person == null)
+                return false;
+
+            return this.ID.Equals(person.ID);
+        }
+    }
+}
---
+++ OverloadOperators/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+
+            employee.FirstName = "Arie";
+            employee.LastName = "Eskinazi";
+            employee.ID = 2;
+
+            Employee employee2 = new Employee();
+            employee2.FirstName = "Dana";
+            employee2.LastName = "Rei";
+            employee2.ID = 1;
+
+            Console.WriteLine(employee.ID == employee2.ID);
+
+            Console.ReadLine();
+
+        }
+    }
+}
